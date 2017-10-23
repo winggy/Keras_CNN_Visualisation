296e8643525207f3d440bf053eb84e86611ac15e
andreacos <andreacos82@gmail.com>
2017-10-23 14:54:57 +0200

    Update KerasVis_jpegnet_flowDir.py

--- KerasVis_jpegnet_flowDir.py
+++ KerasVis_jpegnet_flowDir.py
@@ -12,6 +12,7 @@ from glob import glob
 
 
 def read_hdf5_compressed(hdf5_file):
+    
     """ Reads training and test data and labels from a compressed HDF5 archive. Returns: (N,H,W,C) tuples of training
         and test data, where N is the number of samples and H,W,C are height, width and channels of the image data;
         (N, Nc) tuples of one-hot train and test labels.
@@ -35,6 +36,16 @@ def read_hdf5_compressed(hdf5_file):
 
 
 def make_collage(heatmaps, input_img, test_lowpass=[], pxl_margin=5):
+    
+    """ Creates a composite image by putting side to side the input image and its 
+        class activation maps (once for each class)
+    Keyword arguments:
+    heatmaps   -- KxMxMxD tuple where K=1, [M,N] is the size of the input image, D=3 (RGB channels) x number of classes
+    input_img  -- the input image content (grayscale or RGB)
+    test_lowpass -- the highpass component of the input image
+    pxl_margin -- margins of the white frame around the composite image
+    """
+    
     if input_img.shape[2] < 3:
         input_img_rgb = np.tile(input_img, (1, 1, 3))
 
@@ -42,45 +53,26 @@ def make_collage(heatmaps, input_img, test_lowpass=[], pxl_margin=5):
         test_lowpass = np.reshape(test_lowpass, test_lowpass.shape + (1,))
         test_lowpass = np.tile(test_lowpass, (1, 1, 3))
 
+    # Vertical and horizontal portions of the white frame
     ver_frame = 255 * np.ones((input_img.shape[0], pxl_margin, 3), dtype=heatmaps.dtype)
-
-    collageTop = np.concatenate((ver_frame, input_img_rgb,
-                                 ver_frame, heatmaps[0, :, :, 0:3],
-                                 ver_frame, heatmaps[0, :, :, 3:6],
-                                 #ver_frame, heatmaps[2, :, :, 0:3],
-                                 ver_frame), axis=1)
-
-    #collageBottom = np.concatenate((ver_frame, test_lowpass,
-    #                                ver_frame, heatmaps[0, :, :, 3:6],
-    #                                ver_frame, heatmaps[1, :, :, 3:6],
-    #                                ver_frame, heatmaps[2, :, :, 3:6],
-    #                                ver_frame), axis=1)
-
     hor_frame = 255 * np.ones((pxl_margin, collageTop.shape[1], 3))
 
-    #return np.concatenate((hor_frame, collageTop, hor_frame, collageBottom, hor_frame), axis=0)
-    return np.concatenate((hor_frame, collageTop, hor_frame), axis=0)
-
-
-def make_collage_2(heatmaps, input_img, test_lowpass=[], pxl_margin=5):
-    if input_img.shape[2] < 3:
-        input_img_rgb = np.tile(input_img, (1, 1, 3))
-    else:
-        input_img_rgb = input_img
-
-    if len(test_lowpass.shape) < 3:
-        test_lowpass = np.reshape(test_lowpass, test_lowpass.shape + (1,))
-        test_lowpass = np.tile(test_lowpass, (1, 1, 3))
-
-    ver_frame = 255 * np.ones((input_img.shape[0], pxl_margin, 3), dtype=heatmaps.dtype)
-
-    collageTop = np.concatenate((ver_frame, input_img_rgb,
-                                 ver_frame, heatmaps[0, :, :, 0:3],
-                                 ver_frame), axis=1)
-
-    hor_frame = 255 * np.ones((pxl_margin, collageTop.shape[1], 3))
-
-    return np.concatenate((hor_frame, collageTop, hor_frame), axis=0)
+    # Create collage: a single class activation map
+    if heatmaps.shape[-1] = 3:
+        collage = np.concatenate((ver_frame, input_img_rgb,
+                                  ver_frame, test_lowpass,
+                                  ver_frame, heatmaps[0, :, :, 0:3],
+                                  ver_frame), axis=1)
+        
+    # Create collage: two class activation maps
+    elif heatmaps.shape[-1] = 6: 
+        collage = np.concatenate((ver_frame, input_img_rgb,
+                                  ver_frame, test_lowpass,
+                                  ver_frame, heatmaps[0, :, :, 0:3],
+                                  ver_frame, heatmaps[0, :, :, 3:6],
+                                  ver_frame), axis=1)
+        
+    return np.concatenate((hor_frame, collage, hor_frame), axis=0)
 
 
 if __name__ == '__main__':
@@ -91,6 +83,9 @@ if __name__ == '__main__':
     # Patch size
     IMG_SIZE = 256
 
+    # Base directory
+    workdir = 'C:/Users/utente/PycharmProjects/FiltersVisualisation_Keras/Localisation'
+        
     # ---------------------------------------------------------------------------------------------------------------------
     # DATA PREPARATION
     # ---------------------------------------------------------------------------------------------------------------------
@@ -121,7 +116,6 @@ if __name__ == '__main__':
     # Check accuracy and then compute predictions and extrapolate class labels from them
     accuracy = model.predict(testing_data/255., verbose=1)
     print('Test accuracy {:g}. Loss {:g}'.format(accuracy[1], accuracy[0]))
- 
     predicted_labels = np.argmax(predictions, axis=1)
 
     # Decode one-hot ground-truth labels stored in the input archive
@@ -151,9 +145,6 @@ if __name__ == '__main__':
     # Classes: single JPEG (1) and double JPEG (0)
     classes = [0, 1]
 
-    heatmap_single = np.zeros((N_TEST_IMG, IMG_SIZE, IMG_SIZE, 3), dtype=np.uint8)
-    heatmap_double = np.zeros((N_TEST_IMG, IMG_SIZE, IMG_SIZE, 3), dtype=np.uint8)
-
     print()
     print('Computing localisation maps ... ')
 
@@ -165,31 +156,37 @@ if __name__ == '__main__':
             print('Processed {:6d} of {:6d} images ({:3.1f}%. Elapsed: {:5.1f} seconds)'
                   .format(k, n_files, 100 * k / n_files, time.time() - begin_time))
 
+        # Get nme, class and data of the current image
+        img_class_name = 'single' if decoded_test_labels[k] == 1 else 'double'
+        img_name = os.path.basename(jpeg_files[k])
         test_img = testing_data[k]
 
-        # Localise only for the images that are correctly classified
-        if decoded_test_labels[k] == predicted_labels[k]:
+        # Localisation is carried out only for the images that are correctly classified
+        if decoded_test_labels[k] != predicted_labels[k]:
+            print('  > Image {} detected as {} but it was not'.format(img_name, img_class_name.upper))
+            
+        else:
 
             # Tuple of size (modifiers, img_size, img_size, 3*number of classes), for example: (3, 64, 64, 6). In
             # practice, the i-th, j-th element (i, 64, 64, j) is the heatmap for the i-th modifier relatively
             # j-th class (j has 3 channels since it is a RGB map)
             heatmap_storage = np.zeros((1,) + test_img.shape[:2] + (3 * len(classes),), dtype=np.uint8)
 
-            # Localisation of activating regions given a class cl = 1 (single)
+            # Localisation of activation regions given class cl = 1 (single)
             heatmap = visualize_cam(model,
                                     layer_idx,
                                     filter_indices=1,
                                     seed_input=test_img,
                                     backprop_modifier=modifier)
 
-            # Localisation of activating regions given a class cl = 0 (double)
+            # Localisation of activation regions given class cl = 0 (double)
             heatmap_d = visualize_cam(model,
                                       layer_idx,
                                       filter_indices=0,
                                       seed_input=test_img,
                                       backprop_modifier=modifier)
 
-            # Convert to RGB for display purposes
+            # Convert input image to RGB for display purposes (CAM heatmaps are RGB)
             if test_img.shape[2] < 3:
                 overlayed_map = overlay(np.tile(test_img, (1, 1, 3)), heatmap)
                 overlayed_map_d = overlay(np.tile(test_img, (1, 1, 3)), heatmap_d)
@@ -205,9 +202,6 @@ if __name__ == '__main__':
             # ------------------------------------------------------------------------------
             # Save the detection output as images
             # ------------------------------------------------------------------------------
-            workdir = 'C:/Users/utente/PycharmProjects/FiltersVisualisation_Keras/Localisation'
-            img_class_name = 'single' if decoded_test_labels[k] == 1 else 'double'
-            img_name = os.path.basename(jpeg_files[k])
             scipy.misc.imsave('{}/{}/image_{}_{}_{}.png'.format(workdir,
                                                                 img_class_name, k,
                                                                 img_class_name,
@@ -222,4 +216,4 @@ if __name__ == '__main__':
                                                              img_class_name,
                                                              os.path.splitext(img_name)[0])),
                      gradcam_single=overlayed_map,
-                     gradcam_double=overlayed_map_d)
+                     gradcam_double=overlayed_map_d)           
